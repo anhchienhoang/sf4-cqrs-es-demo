e9d288596eb44b688e69861593a423426e99a72b
Chien Hoang <anhchienhoang@gmail.com>
2018-10-18 18:01:10 +0200

    Fix container startup dependencies

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,28 +2,33 @@
 
 set -xe
 
+cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bk
+cp /etc/nginx/nginx_waiting.conf /etc/nginx/nginx.conf
+/usr/sbin/nginx -g 'daemon on;' &
+
 # Setup dependencies for the first time
 if [ ! -d "/var/www/vendor" ]; then
-    cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bk
-    cp /etc/nginx/nginx_waiting.conf /etc/nginx/nginx.conf
-    /usr/sbin/nginx -g 'daemon on;' &
-
     # Run composer
     cd /var/www && composer install
 
-    # Run composer
-    cd /var/www && composer install
+fi
 
+if [ ! -d "/var/www/node_modules" ]; then
     # Build FE
     cd /var/www && npm install && npm run build
-
-    cp /etc/nginx/nginx.conf.bk /etc/nginx/nginx.conf
-    chown -R www-data:www-data /var/www
-
-    killall -9 nginx
 fi
 
 # Run migrations
 /var/www/bin/console doctrine:migrations:migrate -n
+
+while ! nc -z ${RABBITMQ_HOST} ${RABBITMQ_PORT}; do
+    echo "Waiting for Rabbitmq..."
+    sleep 3;
+done
+
+cp /etc/nginx/nginx.conf.bk /etc/nginx/nginx.conf
+chown -R www-data:www-data /var/www
+
+killall -9 nginx
 
 exec $*
